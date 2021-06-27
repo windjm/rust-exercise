406809f7f5c1b80c894f93eaaee8d8ccb89f3bdf
jiangm-a <idoplay123@163.com>
2021-06-27 17:22:11 +0800

    Remove redundant files

--- source/ls/target/debug/deps/ls.d
+++
@@ -1,5 +0,0 @@
-C:\Users\idopl\Documents\proj\rust-exercise\ls\target\debug\deps\ls.exe: src\main.rs
-
-C:\Users\idopl\Documents\proj\rust-exercise\ls\target\debug\deps\ls.d: src\main.rs
-
-src\main.rs:
--- source/ls/target/debug/ls.d
+++
@@ -1,1 +0,0 @@
-C:\Users\idopl\Documents\proj\rust-exercise\ls\target\debug\ls.exe: C:\Users\idopl\Documents\proj\rust-exercise\ls\src\main.rs
